764d2b05db3dc45c098005b194b747e6324bebeb
justtoconfirm <office@justtoconfirm.co.uk>
2021-03-15 20:09:14 +0000

    About us page added convert all functional components to use ES6 arrow functions

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+    	<>
+    		<h1>About</h1>
+    		<p>This is an example About page</p>
+    	</>
+    )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Home() {
+const Home = () => {
     return <h1>Hello Gatsby</h1>
 }
 
